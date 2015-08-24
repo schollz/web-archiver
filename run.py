26233c931ff5a48e8138e78c0cf6b6a266aa8a0d
Zack Scholl <zack.scholl@gmail.com>
2015-08-24 16:15:04 -0400

    Now can back up repos

--- run.py
+++ run.py
@@ -5,10 +5,10 @@ from selenium import webdriver
 CURRENT_DIRECTORY = os.getcwd()
 
 def archiveSite(site):
-    if not os.path.exists('output/' + site):
-        os.makedirs('output/' + site)
+    if not os.path.exists('sites/' + site):
+        os.makedirs('sites/' + site)
     dateString = datetime.now().strftime('%Y-%m-%d')
-    folder = './output/' + site + '/' + dateString
+    folder = './sites/' + site + '/' + dateString
     if not os.path.exists(folder):
         os.makedirs(folder)
     os.chdir(folder)
@@ -21,7 +21,19 @@ def archiveSite(site):
     browser.quit()
     os.chdir(CURRENT_DIRECTORY)
 
+'''
 with open('sites','r') as f:
-	for site in f:
-		archiveSite(site.strip())
+     for site in f:
+        archiveSite(site.strip())
+'''
 
+with open('repos','r') as f:
+    for repo in f:
+        repo = repo.strip()
+        if not os.path.exists('github'):
+            os.makedirs('github')
+        if not os.path.exists('github/' + repo):
+            os.makedirs('github/' + repo)
+        os.chdir('github/' + repo)
+        os.system('wget https://github.com/schollz/' + repo.strip() + '/archive/master.zip')
+        os.chdir(CURRENT_DIRECTORY)
